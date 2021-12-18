4bc983b434e65d2d4b99e859f85881017cc45821
treatmeniceplz <dawiddobrzycki7@gmail.com>
2021-12-18 15:26:53 -0500

    nuke

--- src/main/java/net/geforcemods/securitycraft/misc/SCManualPage.java
+++
@@ -1,6 +0,0 @@
-package net.geforcemods.securitycraft.misc;
-
-import net.minecraft.network.chat.TranslatableComponent;
-import net.minecraft.world.item.Item;
-
-public record SCManualPage(Item item, TranslatableComponent helpInfo, String designedBy, boolean hasRecipeDescription) {}
--- src/main/java/net/geforcemods/securitycraft/util/ITickingBlockEntity.java
+++
@@ -1,9 +0,0 @@
-package net.geforcemods.securitycraft.util;
-
-import net.minecraft.core.BlockPos;
-import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.state.BlockState;
-
-public interface ITickingBlockEntity {
-	default void tick(Level level, BlockPos pos, BlockState state) {}
-}
